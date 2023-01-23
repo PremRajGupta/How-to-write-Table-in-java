b73910c76c4c5e40653300b65f7aa9e66489e46b
Prem Raj Gupta <79590480+PremRajGupta@users.noreply.github.com>
2023-01-23 13:03:28 +0530

    Add files via upload

---
+++ table.java
@@ -0,0 +1,13 @@
+class table
+{
+    public static void main(String table[])
+	{
+		int s=0,n=1;
+		do
+		{
+			s=s+n;
+			n++;
+		}while(n<=10);
+		System.out.println("Sum of the first trem="+s);
+	}
+}
